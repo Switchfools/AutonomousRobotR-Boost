ca63282d18f3594b58a11a53ed894e818fc7446e
Nicolás Vergara <37335567+Switchfools@users.noreply.github.com>
2018-03-19 18:36:58 -0500

    Add files via upload
    Segunda Versión

--- Control/Posicion.py
+++ Control/Posicion.py
@@ -8,19 +8,6 @@ velocidad_AngularDerecha=0
 RadioRuedas=(3.0/2)
 SeparacionRuedas=13.0
 #Las entradas son las velocidades que salen del control y las reales del robot
-def ControlPWM(VAC,VAR):
-    PWMMIN=-255.0
-    PWMMAX=255.0
-    Error=(VAC-VAR)
-    #ConstanteProporcional del motor
-    K_pM=0.1
-    PWM=K_pM*Error
-    if(np.abs(PWM)<255):
-        return PWM,Error
-    elif(PWM>255):
-        return 255,Error
-    else:
-        return -255,Error
 #se calculan con cada toma de datos, solo se necesita de entrada el numero de vueltas trasncurridas entre cada ejecución del codigo
 def ControlPosicion(Xdeseado,Ydeseado,NumeroVueltasI,NumeroVueltasD):
     Desplazamiento_Izquierda=(2*np.pi*RadioRuedas)*(NumeroVueltasI)
@@ -44,13 +31,13 @@ def ControlPosicion(Xdeseado,Ydeseado,NumeroVueltasI,NumeroVueltasD):
     velocidad_AIzquierdaControl=((2*VelocidadControl)-(AngularVelocity*SeparacionRuedas))/(2*RadioRuedas)
     velocidad_ADerechaControl=((2*VelocidadControl)+(AngularVelocity*SeparacionRuedas))/(2*RadioRuedas)
     # de forma ideal se espera que el error sea aprrox a 0
-    ErrorI=100
-    ErrorD=100
-    while(ErrorI>0.1 and ErrorD>0.1):
-    #PWI,PWD, son los correspondientes PWM que se mandan a cada motor
-        PWI,ErrorI=ControlPWM(velocidad_AIzquierdaControl,velocidad_AIzquierda)
-        PWD,ErrorM=ControlPWM(velocidad_ADerechaControl,velocidad_ADerecha)
-
+    KI=0.3;
+    KD=0.3;
+    PWMI=KI*velocidad_AIzquierdaControl
+    PWMD=KD*velocidad_ADerechaControl
+    Error=np.sqrt((Xdeseado-XRobotNuevo)**2+(Ydeseado-YRobotNuevo)**2)
+    return(PWMI,PWMD,Error)
+    
     Xviejo=XRobotNuevo
     Yviejo=YRobotNuevo
     AnguloRViejo=AnguloRobotNuevo
