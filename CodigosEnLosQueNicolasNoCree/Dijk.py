fc76fda6f0dbd9ec9117d7b66d81935f4f78d52f
Nicolás Vergara <37335567+Switchfools@users.noreply.github.com>
2018-04-30 18:51:57 -0500

    Add files via upload

--- CodigosEnLosQueNicolasNoCree/Dijk.py
+++ CodigosEnLosQueNicolasNoCree/Dijk.py
@@ -1,20 +1,13 @@
+from scipy import misc
 import numpy as np
 from scipy.spatial import Voronoi, voronoi_plot_2d
 import matplotlib.pyplot as plt
-def voro(img):
-    M,N=np.shape(img)
-    Obstaculos=np.zeros([M,N])
-    centros=np.zeros([M,N])
-    Undos=list()
+im = misc.imread('b.png', flatten=True)
+def voro(cuadros, esquinas):
+    Undos=list(cuadros)
+    Undos.append(esquinas)
     Nodos=list()
-    for i in range(1,M-1):
-        for j in range(1,N-1):
-            Med=Obstaculos[i+1,j]+Obstaculos[i+1,j+1]+Obstaculos[i+1,j-1]+Obstaculos[i,j+1]+Obstaculos[i,j-1]+Obstaculos[i-1,j]+Obstaculos[i-1,j+1]+Obstaculos[i-1,j-1]
-            if(Obstaculos[i,j]==255 and (Med==3*(255)or Med==7*(255))):
-                Undos.append([i,j])
-                for l in range(i-1,i+1):
-                    for m in range(j-1,j+1):
-                        centros[i,j]=255
+    print(Undos)
     vor = Voronoi(Undos)
     voronoi_plot_2d(vor)
     plt.show()
@@ -24,3 +17,4 @@ def voro(img):
             if(Obstaculos[int(x),int(y)]!=255):
                 Nodos.append([int(x),int(y)])
     return(Nodos)
+voro(im)
